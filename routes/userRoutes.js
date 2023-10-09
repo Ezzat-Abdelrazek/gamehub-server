02d860f8ebdde16fdf9a980c16478c5881ba668b
Ezzat Abdelrazek <ezzatabdelrazek257@gmail.com>
2023-10-10 00:22:23 +0300

    Add files via upload

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,11 +4,10 @@ const {
   getTopTenUsers,
   updateUserHighestScore,
 } = require("../controllers/userControllers");
-const { protect } = require("../controllers/authControllers");
 
 const { createUser } = require("../controllers/authControllers");
 router.route("/").post(createUser);
-router.route("/:id/score").post(protect, updateUserHighestScore);
+router.route("/:id/score").post(updateUserHighestScore);
 router.route("/top-ten").get(getTopTenUsers);
 
 module.exports = router;
